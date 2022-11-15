c2f4287d65c3a46b7946bae48db81f930ef8b5a4
justin-barrett-bryanisd <justin.barrett@bryanisd.org>
2022-11-15 09:21:56 -0600

    mathC changes

--- src/GradeCreator.java
+++ src/GradeCreator.java
@@ -15,6 +15,7 @@ public class GradeCreator {
     public static void main(String[] args) throws Exception {
         Random randy = new Random();
         int math = randy.nextInt(30)+70, english = randy.nextInt(30)+70, sci = randy.nextInt(30)+70, soc = randy.nextInt(30)+70, pe = randy.nextInt(5)+95, art = randy.nextInt(20)+80;
+        int mathC = randy.nextInt(10), englishC = randy.nextInt(10), sciC = randy.nextInt(10), socC = randy.nextInt(10), peC = randy.nextInt(5), artC = randy.nextInt(8);
 
         int id = randy.nextInt(999999);
         DecimalFormat df = new DecimalFormat("000000");
@@ -30,7 +31,7 @@ public class GradeCreator {
             printLine("PE", pe, out);
             printLine("ART", art, out);
 
-            math += randy.nextInt(6) - 3;
+            math += randy.nextInt(mathC) - mathC/2;
             english += randy.nextInt(10) - 5;
             sci += randy.nextInt(8) - 4;
             soc += randy.nextInt(6) - 3;
